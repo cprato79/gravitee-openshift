2094f1ec2f83ecb31a177d35c3bc15fa0744d189
Claudio Prato <cprato79@gmail.com>
2019-05-22 16:47:11 +0200

    fixed relative path to pv script

--- openshift/persistentvolumes/create_pv.sh
+++ openshift/persistentvolumes/create_pv.sh
@@ -3,5 +3,5 @@
 oc login -u system:admin
 oc delete pv pv0001 pv0002
 
-oc create -f pv0001.yaml
-oc create -f pv0002.yaml
+oc create -f persistentvolumes/pv0001.yaml
+oc create -f persistentvolumes/pv0002.yaml
