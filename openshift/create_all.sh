b3608ec1faf2969db4bde7fad21ee9177dcb37ff
Claudio Prato <cprato79@gmail.com>
2019-05-22 17:27:31 +0200

    added secret to use private git scm

--- openshift/create_all.sh
+++ openshift/create_all.sh
@@ -16,26 +16,42 @@ oc project gravitee
 oc adm policy add-scc-to-user anyuid -z gravitee
 
 oc login -u developer -p developer
+oc project gravitee
 
 # set GRAVITEEIO_VERSION=1.10.4
 export GRAVITEEIO_VERSION=1.26.0
+
+
+add_secret_opt {
+  export GIT_SECRET_OPT="--source-secret=git-cprato-secret"
+
+  # create secret to gravitee project
+  oc create -f secret/create_secret.yml -n gravitee
+  # link secret to build service
+  for i in default deployer builder gravitee; do oc secret link $i git-cprato-secret --for=pull -n gravitee;done
+}
 
 # 1. definie build
 # 2. start building
 # 3. tag this version
 
+add_secret_opt
+
 # gateway
-oc new-build ../ --name=gateway --context-dir=images/gateway/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+#oc new-build ../ --name=gateway --context-dir=images/gateway/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+oc new-build ../ --name=gateway --context-dir=images/gateway/ --strategy=docker $GIT_SECRET_OPT --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc start-build gateway --from-dir ../images/gateway/ --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc tag gateway:latest gateway:$GRAVITEEIO_VERSION
 
 # management-api
-oc new-build ../ --name=management-api --context-dir=images/management-api/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+#oc new-build ../ --name=management-api --context-dir=images/management-api/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+oc new-build ../ --name=management-api --context-dir=images/management-api/ --strategy=docker $GIT_SECRET_OPT --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc start-build management-api --from-dir ../images/management-api/ --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc tag management-api:latest management-api:$GRAVITEEIO_VERSION
 
 # management-ui
-oc new-build ../ --name=management-ui --context-dir=images/management-ui/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+#oc new-build ../ --name=management-ui --context-dir=images/management-ui/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+oc new-build ../ --name=management-ui --context-dir=images/management-ui/ --strategy=docker $GIT_SECRET_OPT --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc start-build management-ui --from-dir ../images/management-ui/ --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc tag management-ui:latest management-ui:$GRAVITEEIO_VERSION
 
