ecc822937d8244d16c5d8cbc2d2a72675eaf8eed
Claudio Prato <cprato79@gmail.com>
2019-05-23 17:16:25 +0200

    removed unused dos scripts

--- openshift/stop_all.cmd
+++
@@ -1,9 +0,0 @@
-oc login -u developer -p developer
-oc project gravitee
-
-# stop instance
-oc scale dc/gateway --replicas=0
-oc scale dc/managementui --replicas=0
-oc scale dc/managementapi --replicas=0
-oc scale dc/elasticsearch --replicas=0
-oc scale dc/mongodb --replicas=0
